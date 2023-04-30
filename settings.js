2def487d761e836d15c15ddbf3b7e33c8d53cac9
Stepan Omelyanenko (Desktop) <ose.26@mail.ru>
2023-04-30 22:47:24 +0300

    1.6. add-result-page

--- settings.js
+++ settings.js
@@ -1,2 +1,11 @@
 // Файл для настроек приложения
 const TIMEOUT = 900;
+
+
+const AppRoute = {
+    Game: 'game',
+    Results: 'results',
+};
+
+window.TIMEOUT = TIMEOUT;
+window.AppRoute = AppRoute;
